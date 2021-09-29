a3e80334871de77c96472d6bed1048989b6c4173
balucp <balucpofficial@gmail.com>
2021-09-29 06:49:56 +0530

    url changes

--- app_student/urls.py
+++ app_student/urls.py
@@ -4,8 +4,7 @@ from app_student import views
 urlpatterns = [
     path('teacher/', views.TeacherAPI.as_view()),
     path('student/', views.StudentAPI.as_view()),
-    path('mark/', views.MarkAPI.as_view()),
-    # path('marklist/', views.MarkListAPI.as_view()),
     path('student/<int:id>/', views.StudentDetailAPI.as_view()),
+    path('mark/', views.MarkAPI.as_view()),
     path('mark/<int:id>/', views.MarkDetailAPI.as_view())
 ]
